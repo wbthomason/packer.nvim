20a77d43f731ae5c0f65b432c4a562057ad60434
Wil Thomason <wil.thomason@gmail.com>
2018-06-05 02:01:48 -0400

    Sketch of sync and use with unimplemented functions

--- lua/plague.lua
+++ lua/plague.lua
@@ -1,5 +1,6 @@
 -- Dependencies
 local uv = require('luv')
+local f = require('fun')
 
 -- Private variables and constants
 local nvim = vim.api -- luacheck: ignore
@@ -21,6 +22,7 @@ plague.config = {
   merge = true,
   plugin_dir = '~/.local/share/nvim/plugins',
   threads = nil,
+  auto_clean = true,
 }
 
 plague.fns = {}
@@ -31,6 +33,10 @@ plague.plugins = nil
 -- Utility functions
 plague.fns.err = function (msg)
   nvim.nvim_err_writeln("[Plague] " .. msg)
+end
+
+plague.fns.echo = function (msg)
+  nvim.nvim_out_write("[Plague] " .. msg .. "\n")
 end
 
 -- Plugin specification functions
@@ -49,6 +55,10 @@ plague.fns.configure = function (config)
 
   if config.threads then
     plague.config.threads = config.threads
+  end
+
+  if config.auto_clean then
+    plague.config.auto_clean = config.auto_clean
   end
 end
 
@@ -85,7 +95,7 @@ plague.fns.sync = function ()
 
   -- Check that there are plugins to install
   if plague.plugins == nil then
-    plague.fns.err("Register plugins with use_plugin first!")
+    plague.fns.err("Register plugins with use() before calling sync!")
     return false
   end
 
@@ -97,23 +107,63 @@ plague.fns.sync = function ()
     events = {},
   }
 
-  local ctx = uv.new_work(
-    function(n) --work,in threadpool
-        local uv = require('luv')
-        local t = uv.thread_self()
-        uv.sleep(100)
-        return n*n,n 
-    end, 
-    function(r,n) print(string.format('%d => %d',n,r)) end    --after work, in loop thread
-)
+  -- Make threadpool context
+  local in_ctx = uv.new_work(
+    function(plug_name, spec)
+      plague.fns.install(spec)
+      return plug_name
+    end,
+    function(plug_name) plague.fns.echo("Installed " .. plug_name) end
+  )
 
-  -- Iterate through the plugins and handle installation
-  for plug_name, spec in pairs(plague.plugins) do
-    if spec.ensure then
-      plague.fns.check_install(plug_name, spec)
-    end
+  local un_ctx = uv.new_work(
+    function(plug_name, spec)
+      plague.fns.uninstall(spec)
+      return plug_name
+    end,
+    function(plug_name) plague.fns.echo("Uninstalled " .. plug_name) end
+  )
+
+  -- Filter out disabled and enabled plugins
+  local disabled_plugins = f.filter(function(_, spec) return spec.disabled end, f.iter(plague.plugins))
+  local enabled_plugins = f.filter(function(_, spec) return not spec.disabled end, f.iter(plague.plugins))
+
+  -- Disable the disabled plugins
+  disabled_plugins:each(plague.fns.disable)
+
+  -- Install enabled but missing plugins
+  local function curried_install(context)
+    return function(name, spec) plague.fns.check_install(name, spec, context) end
   end
+
+  enabled_plugins:filter(function (_, spec) return spec.ensure end):each(curried_install(in_ctx))
+
+  -- Remove uninstalled plugins
+  if plague.config.auto_clean then
+    f.filter(function(name) return plague.plugins[name] end, f.iter(plague.fns.list_packages))
+      :each(function (plug_name, spec) uv.queue_work(un_ctx, plug_name, spec) end)
+  end
+
+  -- Collect hooks
+  local deferred_plugins = enabled_plugins:filter(function(_, spec) return spec.defer end)
+  deferred_plugins:each(plague.fns.gather_hooks)
+
+  -- Set up hooks
+  plague.fns.set_triggers()
 end
 
-plague.fns.use_plugin = function (spec)
+plague.fns.use = function (spec)
+  local name = spec.name
+  if name == nil then
+    name = string.match(spec[1], '[^/]-$')
+    spec.name = name
+  end
+
+  plague.fns.guess_type(spec)
+
+  if plague.plugins == nil then
+    plague.plugins = {}
+  end
+
+  plague.plugins[name] = spec
 end
