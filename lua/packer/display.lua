4cb43cb2a0adb88e741159f309f8695b97d8a831
Wil Thomason <wil.thomason@gmail.com>
2020-03-13 00:49:25 -0400

    Fix off-by-one error in mark positioning and mark position indexing

--- lua/packer/display.lua
+++ lua/packer/display.lua
@@ -1,21 +1,22 @@
-local api     = vim.api
+local vim = vim
+local api = vim.api
 
 local display = {}
 local config = {}
 local display_mt = {
   task_start = function(self, plugin, task)
     vim.fn.appendbufline(self.buf, 1, vim.fn.printf('%s %s %s...', config.working_sym, task, plugin))
-    self.marks[plugin] = api.nvim_buf_set_extmark(self.buf, self.ns, 0, 1, 0, {})
+    self.marks[plugin] = api.nvim_buf_set_extmark(self.buf, self.ns, 0, 2, 0, {})
   end,
   task_succeeded = function(self, plugin, task)
     local line, _ = api.nvim_buf_get_extmark_by_id(self.buf, self.ns, self.marks[plugin])
-    vim.fn.setbufline(self.buf, line, vim.fn.printf('%s %s %s', config.done_sym, task, plugin))
+    vim.fn.setbufline(self.buf, line[1], vim.fn.printf('%s %s %s', config.done_sym, task, plugin))
     api.nvim_buf_del_extmark(self.buf, self.ns, self.marks[plugin])
     self.marks[plugin] = nil
   end,
   task_failed = function(self, plugin, task)
     local line, _ = api.nvim_buf_get_extmark_by_id(self.buf, self.ns, self.marks[plugin])
-    vim.fn.setbufline(self.buf, line, vim.fn.printf('%s %s %s', config.error_sym, task, plugin))
+    vim.fn.setbufline(self.buf, line[1], vim.fn.printf('%s %s %s', config.error_sym, task, plugin))
     api.nvim_buf_del_extmark(self.buf, self.ns, self.marks[plugin])
     self.marks[plugin] = nil
   end,
