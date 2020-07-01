b6e108ba9b1f7a1eadce3ab95fb0cd5465b822c2
Wil Thomason <wil.thomason@gmail.com>
2020-07-01 10:56:27 -0400

    Fix plugin type detection and run lua-format

--- lua/packer/plugin_utils.lua
+++ lua/packer/plugin_utils.lua
@@ -1,16 +1,12 @@
-local a      = require('packer/async')
-local jobs   = require('packer/jobs')
-local util   = require('packer/util')
+local a = require('packer/async')
+local jobs = require('packer/jobs')
+local util = require('packer/util')
 local result = require('packer/result')
-local log    = require('packer/log')
-
-local slice = util.slice
+local log = require('packer/log')
 
 local config = nil
 local plugin_utils = {}
-plugin_utils.cfg = function(_config)
-  config = _config
-end
+plugin_utils.cfg = function(_config) config = _config end
 
 plugin_utils.guess_type = function(plugin)
   if plugin.installer then
@@ -18,11 +14,8 @@ plugin_utils.guess_type = function(plugin)
   elseif vim.fn.isdirectory(plugin.path) ~= 0 then
     plugin.url = plugin.path
     plugin.type = 'local'
-  elseif
-    slice(plugin.path, 1, 6) == 'git://'
-    or slice(plugin.path, 1, 4) == 'http'
-    or string.match(plugin.path, '@')
-  then
+  elseif string.sub(plugin.path, 1, 6) == 'git://' or string.sub(plugin.path, 1, 4) == 'http'
+    or string.match(plugin.path, '@') then
     plugin.url = plugin.path
     plugin.type = 'git'
   else
@@ -71,29 +64,21 @@ plugin_utils.update_rplugins = vim.schedule_wrap(function()
 end)
 
 plugin_utils.ensure_dirs = function()
-  if vim.fn.isdirectory(config.opt_dir) == 0 then
-    vim.fn.mkdir(config.opt_dir, 'p')
-  end
+  if vim.fn.isdirectory(config.opt_dir) == 0 then vim.fn.mkdir(config.opt_dir, 'p') end
 
-  if vim.fn.isdirectory(config.start_dir) == 0 then
-    vim.fn.mkdir(config.start_dir, 'p')
-  end
+  if vim.fn.isdirectory(config.start_dir) == 0 then vim.fn.mkdir(config.start_dir, 'p') end
 end
 
 plugin_utils.find_missing_plugins = function(plugins, opt_plugins, start_plugins)
-  if opt_plugins == nil or  start_plugins == nil then
+  if opt_plugins == nil or start_plugins == nil then
     opt_plugins, start_plugins = plugin_utils.list_installed_plugins()
   end
 
   local missing_plugins = {}
   for _, plugin_name in ipairs(vim.tbl_keys(plugins)) do
     local plugin = plugins[plugin_name]
-    if
-      (not plugin.opt
-      and not start_plugins[util.join_paths(config.start_dir, plugin.short_name)])
-      or (plugin.opt
-      and not opt_plugins[util.join_paths(config.opt_dir, plugin.short_name)])
-    then
+    if (not plugin.opt and not start_plugins[util.join_paths(config.start_dir, plugin.short_name)])
+      or (plugin.opt and not opt_plugins[util.join_paths(config.opt_dir, plugin.short_name)]) then
       table.insert(missing_plugins, plugin_name)
     end
   end
@@ -108,9 +93,7 @@ plugin_utils.load_plugin = function(plugin)
     vim.o.runtimepath = vim.o.runtimepath .. ',' .. plugin.install_path
     for _, pat in ipairs({'plugin/**/*.vim', 'after/plugin/**/*.vim'}) do
       local path = util.join_paths(plugin.install_path, pat)
-      if #vim.fn.glob(path) > 0 then
-        vim.api.nvim_command('silent exe "source ' .. path .. '"')
-      end
+      if #vim.fn.glob(path) > 0 then vim.api.nvim_command('silent exe "source ' .. path .. '"') end
     end
   end
 end
@@ -128,7 +111,7 @@ plugin_utils.post_update_hook = function(plugin, disp)
         if pcall(plugin.run(plugin)) then
           return result.ok(true)
         else
-          return result.err({ msg = 'Error running post update hook' })
+          return result.err({msg = 'Error running post update hook'})
         end
       elseif type(plugin.run) == 'string' then
         if string.sub(plugin.run, 1, 1) == ':' then
@@ -136,17 +119,16 @@ plugin_utils.post_update_hook = function(plugin, disp)
           vim.api.nvim_command(string.sub(plugin.run, 2))
           return result.ok(true)
         else
-          local hook_output = { err = {}, output = {} }
+          local hook_output = {err = {}, output = {}}
           local hook_callbacks = {
             stderr = jobs.logging_callback(hook_output.err, hook_output.output),
-            stdout = jobs.logging_callback(hook_output.err, hook_output.output, nil, disp, plugin_name)
+            stdout = jobs.logging_callback(hook_output.err, hook_output.output, nil, disp,
+                                           plugin_name)
           }
           local cmd = {
-            os.getenv('SHELL'),
-            '-c',
-            'cd ' .. plugin.install_path .. ' && ' .. plugin.run
+            os.getenv('SHELL'), '-c', 'cd ' .. plugin.install_path .. ' && ' .. plugin.run
           }
-          return a.wait(jobs.run(cmd , { capture_output = hook_callbacks }))
+          return a.wait(jobs.run(cmd, {capture_output = hook_callbacks}))
         end
       else
         return a.wait(jobs.run(plugin.run))
