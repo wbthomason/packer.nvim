216d1614dfb93388233c19d79e80d255e6607b0f
Wil Thomason <wil.thomason@gmail.com>
2020-07-11 11:02:41 -0400

    Fix #3 by removing use of __index metamethod and deep-extending config table

--- lua/packer.lua
+++ lua/packer.lua
@@ -2,52 +2,51 @@
 -- TODO: Fetch/manage LuaRocks dependencies
 -- TODO: Performance analysis/tuning
 -- TODO: Merge start plugins?
-
 -- WIP:
 -- TODO: Allow separate packages
-
-local a            = require('packer/async')
-local clean        = require('packer/clean')
-local compile      = require('packer/compile')
-local display      = require('packer/display')
-local handlers     = require('packer/handlers')
-local install      = require('packer/install')
-local log          = require('packer/log')
+local a = require('packer/async')
+local clean = require('packer/clean')
+local compile = require('packer/compile')
+local display = require('packer/display')
+local handlers = require('packer/handlers')
+local install = require('packer/install')
+local log = require('packer/log')
 local plugin_types = require('packer/plugin_types')
 local plugin_utils = require('packer/plugin_utils')
-local update       = require('packer/update')
-local util         = require('packer/util')
+local update = require('packer/update')
+local util = require('packer/util')
 
-local async        = a.sync
-local await        = a.wait
+local async = a.sync
+local await = a.wait
 
 -- Config
 local packer = {}
 local config_defaults = {
-  ensure_dependencies   = true,
-  package_root   = util.is_windows and '~\\AppData\\Local\\nvim-data\\site\\pack' or '~/.local/share/nvim/site/pack',
+  ensure_dependencies = true,
+  package_root = util.is_windows and '~\\AppData\\Local\\nvim-data\\site\\pack'
+    or '~/.local/share/nvim/site/pack',
   plugin_package = 'packer',
   max_jobs = nil,
   auto_clean = true,
   git = {
     cmd = 'git',
     subcommands = {
-      update         = '-C %s pull --ff-only --progress --rebase=false',
-      install        = 'clone %s %s --depth %i --no-single-branch --progress',
-      fetch          = '-C %s fetch --depth 999999 --progress',
-      checkout       = '-C %s checkout %s --',
-      update_branch  = '-C %s merge --ff-only @{u}',
+      update = '-C %s pull --ff-only --progress --rebase=false',
+      install = 'clone %s %s --depth %i --no-single-branch --progress',
+      fetch = '-C %s fetch --depth 999999 --progress',
+      checkout = '-C %s checkout %s --',
+      update_branch = '-C %s merge --ff-only @{u}',
       current_branch = '-C %s branch --show-current',
-      diff           = '-C %s log --color=never --pretty=format:FMT --no-show-signature HEAD@{1}...HEAD',
-      diff_fmt       = '%%h %%s (%%cr)',
-      get_rev        = '-C %s rev-parse --short HEAD',
-      get_msg        = '-C %s log --color=never --pretty=format:FMT --no-show-signature HEAD -n 1',
-      submodules     = '-C %s submodule update --init --recursive --progress'
+      diff = '-C %s log --color=never --pretty=format:FMT --no-show-signature HEAD@{1}...HEAD',
+      diff_fmt = '%%h %%s (%%cr)',
+      get_rev = '-C %s rev-parse --short HEAD',
+      get_msg = '-C %s log --color=never --pretty=format:FMT --no-show-signature HEAD -n 1',
+      submodules = '-C %s submodule update --init --recursive --progress'
     },
-    depth = 1,
+    depth = 1
   },
   display = {
-    open_fn  = nil,
+    open_fn = nil,
     open_cmd = '65vnew [packer]',
     working_sym = '🔄',
     error_sym = '❌',
@@ -60,15 +59,13 @@ local config_defaults = {
   }
 }
 
-local config_mt = { __index = config_defaults }
-local config    = setmetatable({}, config_mt)
+local config = vim.tbl_extend('force', {}, config_defaults)
 
 local plugins = nil
 
 packer.init = function(user_config)
   user_config = user_config or {}
-  config = vim.tbl_extend('force', config, user_config)
-  setmetatable(config, config_mt)
+  config = util.deep_extend('force', config, user_config)
 
   packer.reset()
 
@@ -79,30 +76,17 @@ packer.init = function(user_config)
   config.start_dir = util.join_paths(config.pack_dir, 'start')
 
   for _, mod in ipairs({
-    clean,
-    compile,
-    display,
-    handlers,
-    install,
-    plugin_types,
-    plugin_utils,
-    update
-  }) do
-    mod.cfg(config)
-  end
+    clean, compile, display, handlers, install, plugin_types, plugin_utils, update
+  }) do mod.cfg(config) end
 
   plugin_utils.ensure_dirs()
 end
 
-packer.reset = function()
-  plugins = {}
-end
+packer.reset = function() plugins = {} end
 
 local manage = nil
 manage = function(plugin)
-  if type(plugin) == 'string' then
-    plugin = { plugin }
-  end
+  if type(plugin) == 'string' then plugin = {plugin} end
 
   local path = vim.fn.expand(plugin[1])
   local name_segments = vim.split(path, '/')
@@ -117,9 +101,7 @@ manage = function(plugin)
   plugin.path = path
 
   -- Some config keys modify a plugin type
-  if plugin.opt then
-    plugin.manual_opt = true
-  end
+  if plugin.opt then plugin.manual_opt = true end
 
   for _, key in ipairs(compile.opt_keys) do
     if plugin[key] then
@@ -129,55 +111,38 @@ manage = function(plugin)
   end
 
   -- TODO: This needs to change for supporting multiple packages
-  plugin.install_path = util.join_paths(plugin.opt and config.opt_dir or config.start_dir, plugin.short_name)
+  plugin.install_path = util.join_paths(plugin.opt and config.opt_dir or config.start_dir,
+                                        plugin.short_name)
 
-  if not plugin.type then
-    plugin_utils.guess_type(plugin)
-  end
+  if not plugin.type then plugin_utils.guess_type(plugin) end
 
-  if plugin.type ~= 'custom' then
-    plugin_types[plugin.type].setup(plugin)
-  end
+  if plugin.type ~= 'custom' then plugin_types[plugin.type].setup(plugin) end
 
-  for k, v in pairs(plugin) do
-    if handlers[k] then
-      handlers[k](plugins, plugin, v)
-    end
-  end
+  for k, v in pairs(plugin) do if handlers[k] then handlers[k](plugins, plugin, v) end end
 
   plugins[name] = plugin
 
   if plugin.requires and config.ensure_dependencies then
-    if type(plugin.requires) == 'string' then
-      plugin.requires = { plugin.requires }
-    end
+    if type(plugin.requires) == 'string' then plugin.requires = {plugin.requires} end
 
     for _, req in ipairs(plugin.requires) do
-      if type(req) == 'string' then
-        req = { req }
-      end
+      if type(req) == 'string' then req = {req} end
 
       local req_name_segments = vim.split(req[1], '/')
       local req_name = req_name_segments[#req_name_segments]
-      if not plugins[req_name] then
-        manage(req)
-      end
+      if not plugins[req_name] then manage(req) end
     end
   end
 end
 
-packer.set_handler = function(name, func)
-  handlers[name] = func
-end
+packer.set_handler = function(name, func) handlers[name] = func end
 
 -- Add a plugin to the managed set
 packer.use = manage
 
 packer.clean = function(results) async(function() await(clean(plugins, results)) end)() end
 
-local function args_or_all(...)
-  return util.nonempty_or({...}, vim.tbl_keys(plugins))
-end
+local function args_or_all(...) return util.nonempty_or({...}, vim.tbl_keys(plugins)) end
 
 packer.install = function(...)
   local install_plugins
@@ -199,13 +164,12 @@ packer.install = function(...)
     if next(tasks) then
       table.insert(tasks, 1, function() return not display.status.running end)
       table.insert(tasks, 1, config.max_jobs and config.max_jobs or (#tasks - 1))
-      display_win:update_headline_message('installing ' .. #tasks - 2 .. ' / ' .. #tasks - 2 .. ' plugins')
+      display_win:update_headline_message('installing ' .. #tasks - 2 .. ' / ' .. #tasks - 2
+                                            .. ' plugins')
       a.interruptible_wait_pool(unpack(tasks))
       local install_paths = {}
       for plugin_name, r in pairs(results.installs) do
-        if r.ok then
-          table.insert(install_paths, results.plugins[plugin_name].install_path)
-        end
+        if r.ok then table.insert(install_paths, results.plugins[plugin_name].install_path) end
       end
 
       await(a.main)
@@ -222,12 +186,11 @@ end
 packer.update = function(...)
   local update_plugins = args_or_all(...)
   async(function()
-    local start_time                         = vim.fn.reltime()
-    local results                            = {}
+    local start_time = vim.fn.reltime()
+    local results = {}
     local missing_plugins, installed_plugins = util.partition(
-      plugin_utils.find_missing_plugins(plugins),
-      update_plugins
-    )
+                                                 plugin_utils.find_missing_plugins(plugins),
+                                                 update_plugins)
 
     update.fix_plugin_types(plugins, missing_plugins, results)
     local _
@@ -238,21 +201,16 @@ packer.update = function(...)
     vim.list_extend(tasks, update_tasks)
     table.insert(tasks, 1, function() return not display.status.running end)
     table.insert(tasks, 1, config.max_jobs and config.max_jobs or (#tasks - 1))
-    display_win:update_headline_message(
-      'updating ' .. #tasks - 2 .. ' / ' .. #tasks - 2 .. ' plugins'
-    )
+    display_win:update_headline_message('updating ' .. #tasks - 2 .. ' / ' .. #tasks - 2
+                                          .. ' plugins')
     a.interruptible_wait_pool(unpack(tasks))
     local install_paths = {}
     for plugin_name, r in pairs(results.installs) do
-      if r.ok then
-        table.insert(install_paths, results.plugins[plugin_name].install_path)
-      end
+      if r.ok then table.insert(install_paths, results.plugins[plugin_name].install_path) end
     end
 
     for plugin_name, r in pairs(results.updates) do
-      if r.ok then
-        table.insert(install_paths, results.plugins[plugin_name].install_path)
-      end
+      if r.ok then table.insert(install_paths, results.plugins[plugin_name].install_path) end
     end
 
     await(a.main)
@@ -266,12 +224,11 @@ end
 packer.sync = function(...)
   local sync_plugins = args_or_all(...)
   async(function()
-    local start_time                         = vim.fn.reltime()
-    local results                            = {}
+    local start_time = vim.fn.reltime()
+    local results = {}
     local missing_plugins, installed_plugins = util.partition(
-      plugin_utils.find_missing_plugins(plugins),
-      sync_plugins
-    )
+                                                 plugin_utils.find_missing_plugins(plugins),
+                                                 sync_plugins)
 
     update.fix_plugin_types(plugins, missing_plugins, results)
     local _
@@ -287,19 +244,16 @@ packer.sync = function(...)
     vim.list_extend(tasks, update_tasks)
     table.insert(tasks, 1, function() return not display.status.running end)
     table.insert(tasks, 1, config.max_jobs and config.max_jobs or (#tasks - 1))
-    display_win:update_headline_message('syncing ' .. #tasks - 2 .. ' / ' .. #tasks - 2 .. ' plugins')
+    display_win:update_headline_message(
+      'syncing ' .. #tasks - 2 .. ' / ' .. #tasks - 2 .. ' plugins')
     a.interruptible_wait_pool(unpack(tasks))
     local install_paths = {}
     for plugin_name, r in pairs(results.installs) do
-      if r.ok then
-        table.insert(install_paths, results.plugins[plugin_name].install_path)
-      end
+      if r.ok then table.insert(install_paths, results.plugins[plugin_name].install_path) end
     end
 
     for plugin_name, r in pairs(results.updates) do
-      if r.ok then
-        table.insert(install_paths, results.plugins[plugin_name].install_path)
-      end
+      if r.ok then table.insert(install_paths, results.plugins[plugin_name].install_path) end
     end
 
     await(a.main)
--- lua/packer/util.lua
+++ lua/packer/util.lua
@@ -2,18 +2,14 @@ local util = {}
 
 util.map = function(func, seq)
   local result = {}
-  for _, v in ipairs(seq) do
-    table.insert(result, func(v))
-  end
+  for _, v in ipairs(seq) do table.insert(result, func(v)) end
 
   return result
 end
 
 util.imap = function(func, seq)
   local result = {}
-  for k, v in pairs(seq) do
-    table.insert(result, func(k, v))
-  end
+  for k, v in pairs(seq) do table.insert(result, func(k, v)) end
 
   return result
 end
@@ -24,9 +20,7 @@ util.zip = function(...)
   local min_length = math.min(unpack(util.map(function(l) return #l end, args)))
   for i = 1, min_length do
     local elem = {}
-    for _, l in ipairs(args) do
-      table.insert(elem, l[i])
-    end
+    for _, l in ipairs(args) do table.insert(elem, l[i]) end
 
     table.insert(result, elem)
   end
@@ -34,23 +28,15 @@ util.zip = function(...)
   return result
 end
 
-util.tail = function(seq)
-  return { unpack(seq, 2, #seq) }
-end
+util.tail = function(seq) return {unpack(seq, 2, #seq)} end
 
-util.head = function(seq)
-  return seq[1]
-end
+util.head = function(seq) return seq[1] end
 
 util.fold = function(func, seq, init)
   local acc = init or seq[1]
-  if init == nil then
-    seq = util.tail(seq)
-  end
+  if init == nil then seq = util.tail(seq) end
 
-  for _, v in ipairs(seq) do
-    acc = func(acc, v)
-  end
+  for _, v in ipairs(seq) do acc = func(acc, v) end
 
   return acc
 end
@@ -59,18 +45,14 @@ util.slice = function(seq, start, endpoint, step)
   local result = {}
   endpoint = endpoint or #seq
   step = step or 1
-  for i = start, endpoint, step do
-    table.insert(result, seq[i])
-  end
+  for i = start, endpoint, step do table.insert(result, seq[i]) end
 
   return result
 end
 
 util.filter = function(func, seq)
   local function f(acc, val)
-    if func(val) then
-      table.insert(acc, val)
-    end
+    if func(val) then table.insert(acc, val) end
 
     return acc
   end
@@ -80,11 +62,9 @@ end
 
 util.partition = function(sub, seq)
   local sub_vals = {}
-  for _, val in ipairs(sub) do
-    sub_vals[val] = true
-  end
+  for _, val in ipairs(sub) do sub_vals[val] = true end
 
-  local result = { {}, {} }
+  local result = {{}, {}}
   for _, val in ipairs(seq) do
     if sub_vals[val] then
       table.insert(result[1], val)
@@ -106,17 +86,13 @@ end
 
 util.get_keys = function(tbl)
   local keys = {}
-  for k, _ in pairs(tbl) do
-    table.insert(keys, k)
-  end
+  for k, _ in pairs(tbl) do table.insert(keys, k) end
   return keys
 end
 
 util.is_windows = jit.os == 'Windows'
 local function get_separator()
-  if util.is_windows then
-    return '\\'
-  end
+  if util.is_windows then return '\\' end
 
   return '/'
 end
@@ -132,18 +108,49 @@ util.get_plugin_full_name = function(plugin)
     plugin_name = plugin_name .. '/' .. plugin.branch
   end
 
-  if plugin.rev then
-    plugin_name = plugin_name .. '@' .. plugin.rev
-  end
+  if plugin.rev then plugin_name = plugin_name .. '@' .. plugin.rev end
 
   return plugin_name
 end
 
 util.memoize = function(func)
   return setmetatable({}, {
-    __index = function(self, k) local v = func(k); self[k] = v; return v end,
+    __index = function(self, k)
+      local v = func(k);
+      self[k] = v;
+      return v
+    end,
     __call = function(self, k) return self[k] end
   })
+end
+
+util.deep_extend = function(policy, ...)
+  local result = {}
+  local function helper(policy, k, v1, v2)
+    if type(v1) ~= 'table' or type(v2) ~= 'table' then
+      if policy == 'error' then
+        error('Key ' .. vim.inspect(k) .. ' is already present with value ' .. vim.inspect(v1))
+      elseif policy == 'force' then
+        return v2
+      else
+        return v1
+      end
+    else
+      return util.deep_extend(policy, v1, v2)
+    end
+  end
+
+  for _, t in ipairs({...}) do
+    for k, v in pairs(t) do
+      if result[k] ~= nil then
+        result[k] = helper(policy, k, result[k], v)
+      else
+        result[k] = v
+      end
+    end
+  end
+
+  return result
 end
 
 return util
