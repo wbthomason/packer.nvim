5bb072a0c696cc2444d169889d304719864e7ff7
TJ DeVries <devries.timothyj@gmail.com>
2020-07-21 10:25:27 -0400

    WIP

--- lua/packer/startup.lua
+++ lua/packer/startup.lua
@@ -1,13 +1,13 @@
 local startup = {}
 
 startup.initialize_default_mappings = function(files)
-  vim.cmd [[command! PackerInstall packadd packer.nvim | lua require('plugins').install()]]
-  vim.cmd [[command! PackerUpdate packadd packer.nvim | lua require('plugins').update()]]
-  vim.cmd [[command! PackerSync packadd packer.nvim | lua require('plugins').sync()]]
-  vim.cmd [[command! PackerClean packadd packer.nvim | lua require('plugins').clean()]]
+  vim.cmd [[command! PackerInstall  lua require('plugins').install()]]
+  vim.cmd [[command! PackerUpdate   lua require('plugins').update()]]
+  vim.cmd [[command! PackerSync     lua require('plugins').sync()]]
+  vim.cmd [[command! PackerClean    lua require('plugins').clean()]]
   vim.cmd(string.format(
-    [[command! PackerCompile packadd packer.nvim | lua require('plugins').compile('%s')]],
-    files.plugin_file or vim.fn.stdpath('config') .. '/plugin/packer_load.vim'
+    [[command! PackerCompile lua require('plugins').compile('%s')]],
+    files.compile_location or vim.fn.stdpath('config') .. '/plugin/packer_compiled.vim'
   ))
 end
 
