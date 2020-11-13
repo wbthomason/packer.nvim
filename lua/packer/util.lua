647a6e8cb82e4c2e955be41da56f704398dcf208
Wil Thomason <wil.thomason@gmail.com>
2020-11-13 12:00:48 -0500

    Add default floating window function to close #75

--- lua/packer/util.lua
+++ lua/packer/util.lua
@@ -95,4 +95,33 @@ util.deep_extend = function(policy, ...)
   return result
 end
 
+-- Credit to @crs for the original function
+util.float = function()
+  local columns = vim.o.columns
+  local rows = vim.o.lines
+  local width = math.min(columns - 4, math.max(80, columns - 20))
+  local height = math.min(rows - 4, math.max(20, rows - 10))
+  local top = ((rows - height) / 2) - 1
+  local left = (columns - width) / 2
+  local opts = {
+    relative = 'editor',
+    row = top,
+    col = left,
+    width = width,
+    height = height,
+    style = 'minimal'
+  }
+  local bg_buf = vim.api.nvim_create_buf(false, true)
+  local bg_win = vim.api.nvim_open_win(bg_buf, true, opts)
+  vim.api.nvim_win_set_option(bg_win, 'winhl', 'Normal:NormalFloat')
+  opts.row = opts.row + 1
+  opts.height = opts.height - 2
+  opts.col = opts.col + 2
+  opts.width = opts.width - 4
+  local buf = vim.api.nvim_create_buf(false, true)
+  local win = vim.api.nvim_open_win(buf, true, opts)
+  vim.cmd([[autocmd! BufWipeout <buffer> lua vim.api.nvim_buf_delete(]] .. bg_buf .. ', {})')
+  return true, win, buf
+end
+
 return util
