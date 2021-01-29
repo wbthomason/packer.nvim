0df654da55d207b21289d7c973d2bfda010624ef
Wil Thomason <wil.thomason@gmail.com>
2021-01-29 11:20:39 -0500

    Maybe fix #185?

--- lua/packer/display.lua
+++ lua/packer/display.lua
@@ -223,7 +223,9 @@ local display_mt = {
     local item_order = {}
     local rocks_items = {}
     if results.removals then
-      for _, plugin_dir in pairs(results.removals) do
+      print(vim.inspect(results.removals))
+      for _, plugin_dir in ipairs(results.removals) do
+        table.insert(item_order, plugin_dir)
         table.insert(raw_lines, fmt(' %s Removed %s', config.removed_sym, plugin_dir))
       end
     end
