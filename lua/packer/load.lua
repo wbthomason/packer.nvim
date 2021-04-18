2039105b9d7278a2c32659bc3718ebc489dd59ec
Wil Thomason <wil.thomason@gmail.com>
2021-04-18 01:46:04 -0400

    Fix: remove redundant wanted_plugin line from loader

--- lua/packer/load.lua
+++ lua/packer/load.lua
@@ -15,10 +15,7 @@ packer_load = function(names, cause, plugins)
       needs_bufread = needs_bufread or plugin.needs_bufread
 
       if plugin.wants then
-        for _, wanted_name in ipairs(plugin.wants) do
-          local wanted_plugin = plugins[wanted_name]
-          packer_load({wanted_name}, {}, plugins)
-        end
+        for _, wanted_name in ipairs(plugin.wants) do packer_load({wanted_name}, {}, plugins) end
       end
 
       if plugin.commands then
