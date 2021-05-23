84aba1cef7deb95382f67cbee5d260a833012909
Timothée Sterle <timothee-s@mailoo.org>
2021-05-23 12:24:44 +0200

    refactor(local): use libuv to create symlinks

--- lua/packer/plugin_types/local.lua
+++ lua/packer/plugin_types/local.lua
@@ -1,5 +1,4 @@
 local a = require('packer.async')
-local jobs = require('packer.jobs')
 local log = require('packer.log')
 local util = require('packer.util')
 local result = require('packer.result')
@@ -9,29 +8,23 @@ local await = a.wait
 
 local config = nil
 local function cfg(_config) config = _config end
-local job_opts = {capture_output = true}
+
+local symlink = a.wrap(vim.loop.fs_symlink)
 
 local function setup_local(plugin)
   local from = plugin.path
   local to = plugin.install_path
-  local task
-
-  if vim.fn.executable('ln') == 1 then
-    task = {'ln', '-sf', from, to}
-    -- NOTE: We assume mklink is present on Windows because executable() is apparently not reliable
-    -- (see issue #49)
-  elseif util.is_windows then
-    task = {'cmd', '/C', 'mklink', '/d', to, from}
-  else
-    log.error('No executable symlink command found!')
-    return
-  end
 
   local plugin_name = util.get_plugin_full_name(plugin)
   plugin.installer = function(disp)
     return async(function()
       disp:task_update(plugin_name, 'making symlink...')
-      return await(jobs.run(task, job_opts))
+      local err, success = await(symlink(from, to, {dir = true}))
+      if not success then
+        plugin.output = {err = {err}}
+        return result.err(err)
+      end
+      return result.ok()
     end)
   end
 
