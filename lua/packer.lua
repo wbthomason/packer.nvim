b04082e8526390b92da1e3bda59acb82f6fb281c
Wil Thomason <wil.thomason@gmail.com>
2021-07-01 11:43:17 -0400

    Add .git suffix to default URL format string

--- lua/packer.lua
+++ lua/packer.lua
@@ -39,7 +39,7 @@ local config_defaults = {
     },
     depth = 1,
     clone_timeout = 60,
-    default_url_format = 'https://github.com/%s'
+    default_url_format = 'https://github.com/%s.git'
   },
   display = {
     non_interactive = false,
