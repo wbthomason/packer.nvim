2f531913cfb73db18fec8e2d159d26680c12522e
kevinhwang91 <kevin.hwang@live.com>
2021-07-13 19:48:11 +0800

    feat(compile): support ftdetect for lua
    support ftdetect for lua

--- lua/packer/compile.lua
+++ lua/packer/compile.lua
@@ -212,8 +212,8 @@ local function detect_after_plugin(name, plugin_path)
 end
 
 local ftdetect_patterns = {
-  table.concat({ 'ftdetect', '**/*.vim' }, util.get_separator()),
-  table.concat({ 'after', 'ftdetect', '**/*.vim' }, util.get_separator()),
+  table.concat({ 'ftdetect', [[**/*.\(vim\|lua\)]] }, util.get_separator()),
+  table.concat({ 'after', 'ftdetect', [[**/*.\(vim\|lua\)]] }, util.get_separator()),
 }
 local function detect_ftdetect(name, plugin_path)
   local paths = {
