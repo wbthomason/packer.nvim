wbthomason/packer.nvim

commit 755f7c4a1c4d92da7cda947efa5e5aed1d010a3a

feat: allow using other lazy loaders along with cond (#609)
* feat: allow using other lazy loaders along with cond

* refactor: split packer_load into smaller functions

* enhance: remove loaders when cond fails too + allow PackerLoad to force unconditional plugin load

* Allow boolean values for cond

* address review issues

* use boolean value for bang in packer.loader

Co-authored-by: Wil Thomason <wil.thomason@gmail.com>

lua/packer.lua
@@ -130,7 +130,7 @@ packer.make_commands = function()
   vim.cmd [[command! -nargs=* PackerCompile  lua require('packer').compile(<q-args>)]]
   vim.cmd [[command! PackerStatus            lua require('packer').status()]]
   vim.cmd [[command! PackerProfile           lua require('packer').profile_output()]]
-  vim.cmd [[command! -nargs=+ -complete=customlist,v:lua.require'packer'.loader_complete PackerLoad lua require('packer').loader(<q-args>)]]
+  vim.cmd [[command! -bang -nargs=+ -complete=customlist,v:lua.require'packer'.loader_complete PackerLoad lua require('packer').loader(<f-args>, '<bang>' == '!')]]
 end
 
 packer.reset = function()
@@ -226,7 +226,7 @@ manage = function(plugin_data)
 
   local compile = require_and_configure 'compile'
   for _, key in ipairs(compile.opt_keys) do
-    if plugin_spec[key] then
+    if plugin_spec[key] ~= nil then
       plugin_spec.opt = true
       break
     end
@@ -745,11 +745,13 @@ end
 -- Load plugins
 -- @param plugins string String of space separated plugins names
 --                      intended for PackerLoad command
-packer.loader = function(plugins_names)
-  local plugin_list = vim.tbl_filter(function(name)
-    return #name > 0
-  end, vim.split(plugins_names, ' '))
-  require 'packer.load'(plugin_list, {}, _G.packer_plugins)
+packer.loader = function(...)
+  local plugin_list = { ... }
+  local force = plugin_list[#plugin_list] == true
+  if type(plugin_list[#plugin_list]) == 'boolean' then
+    plugin_list[#plugin_list] = nil
+  end
+  require 'packer.load'(plugin_list, {}, _G.packer_plugins, force)
 end
 
 -- Completion for not yet loaded plugins

lua/packer/compile.lua
@@ -164,22 +164,6 @@ local function timed_chunk(chunk, name, output_table)
   return output_table
 end
 
--- We special-case timing for conditionals because we can't directly wrap the condition test
-local function timed_condition(condition, body, name, output_table)
-  output_table = output_table or {}
-  local condition_time_call = 'time("' .. name .. '", '
-  output_table[#output_table + 1] = condition_time_call .. 'true)'
-  output_table[#output_table + 1] = 'if'
-  output_table[#output_table + 1] = condition
-  output_table[#output_table + 1] = 'then'
-  output_table[#output_table + 1] = condition_time_call .. 'false)'
-  output_table[#output_table + 1] = body
-  output_table[#output_table + 1] = 'else'
-  output_table[#output_table + 1] = condition_time_call .. 'false)'
-  output_table[#output_table + 1] = 'end'
-  return output_table
-end
-
 local function dump_loaders(loaders)
   local result = vim.deepcopy(loaders)
   for k, _ in pairs(result) do
@@ -264,7 +248,6 @@ local function make_loaders(_, plugins, output_lua, should_profile)
   local fts = {}
   local events = {}
   local condition_ids = {}
-  local condition_bytecode = {}
   local commands = {}
   local keymaps = {}
   local after = {}
@@ -324,9 +307,33 @@ local function make_loaders(_, plugins, output_lua, should_profile)
         setup[name] = plugin.setup
       end
 
+      -- Keep this as first opt loader to maintain only_cond ?
+      if plugin.cond ~= nil then
+        loaders[name].only_sequence = false
+        loaders[name].only_setup = false
+        loaders[name].only_cond = true
+        if type(plugin.cond) ~= 'table' then
+          plugin.cond = { plugin.cond }
+        end
+
+        for _, condition in ipairs(plugin.cond) do
+          loaders[name].cond = {}
+          if type(condition) == 'function' then
+            _, condition = make_try_loadstring(condition, 'condition', name)
+          elseif type(condition) == 'string' then
+            condition = 'return ' .. condition
+          end
+
+          condition_ids[condition] = condition_ids[condition] or {}
+          table.insert(loaders[name].cond, condition)
+          table.insert(condition_ids[condition], name)
+        end
+      end
+
       if plugin.ft then
         loaders[name].only_sequence = false
         loaders[name].only_setup = false
+        loaders[name].only_cond = false
         vim.list_extend(ftdetect_paths, detect_ftdetect(name, loaders[name].path))
         if type(plugin.ft) == 'string' then
           plugin.ft = { plugin.ft }
@@ -340,6 +347,7 @@ local function make_loaders(_, plugins, output_lua, should_profile)
       if plugin.event then
         loaders[name].only_sequence = false
         loaders[name].only_setup = false
+        loaders[name].only_cond = false
         if type(plugin.event) == 'string' then
           if not plugin.event:find '%s' then
             plugin.event = { plugin.event .. ' *' }
@@ -357,27 +365,10 @@ local function make_loaders(_, plugins, output_lua, should_profile)
         end
       end
 
-      if plugin.cond then
-        loaders[name].only_sequence = false
-        loaders[name].only_setup = false
-        if type(plugin.cond) == 'string' or type(plugin.cond) == 'function' then
-          plugin.cond = { plugin.cond }
-        end
-
-        for _, condition in ipairs(plugin.cond) do
-          if type(condition) == 'function' then
-            condition = vim.inspect(string.dump(condition, true))
-            condition_bytecode[condition] = true
-          end
-
-          condition_ids[condition] = condition_ids[condition] or {}
-          table.insert(condition_ids[condition], name)
-        end
-      end
-
       if plugin.cmd then
         loaders[name].only_sequence = false
         loaders[name].only_setup = false
+        loaders[name].only_cond = false
         if type(plugin.cmd) == 'string' then
           plugin.cmd = { plugin.cmd }
         end
@@ -393,6 +384,7 @@ local function make_loaders(_, plugins, output_lua, should_profile)
       if plugin.keys then
         loaders[name].only_sequence = false
         loaders[name].only_setup = false
+        loaders[name].only_cond = false
         if type(plugin.keys) == 'string' then
           plugin.keys = { plugin.keys }
         end
@@ -442,6 +434,7 @@ local function make_loaders(_, plugins, output_lua, should_profile)
       if plugin.module or plugin.module_pattern then
         loaders[name].only_sequence = false
         loaders[name].only_setup = false
+        loaders[name].only_cond = false
 
         if plugin.module then
           if type(plugin.module) == 'string' then
@@ -523,33 +516,16 @@ local function make_loaders(_, plugins, output_lua, should_profile)
   end
 
   local conditionals = {}
-  for condition, names in pairs(condition_ids) do
-    local conditional_loads = {}
+  for _, names in pairs(condition_ids) do
     for _, name in ipairs(names) do
-      timed_chunk(
-        fmt('\trequire("packer.load")({"%s"}, {}, _G.packer_plugins)', name),
-        'packadd for ' .. name,
-        conditional_loads
-      )
-      if plugins[name].config then
-        local lines = { '-- Config for: ' .. name }
-        timed_chunk(plugins[name].executable_config, 'Config for ' .. name, lines)
-        vim.list_extend(conditional_loads, lines)
+      if loaders[name].only_cond then
+        timed_chunk(
+          fmt('  require("packer.load")({"%s"}, {}, _G.packer_plugins)', name),
+          'Conditional loading of ' .. name,
+          conditionals
+        )
       end
     end
-    local executable_conditional = condition
-    if condition_bytecode[condition] then
-      executable_conditional = 'try_loadstring(' .. condition .. ', "condition", \'' .. vim.inspect(names) .. "')"
-    end
-
-    vim.list_extend(
-      conditionals,
-      timed_condition(
-        executable_conditional,
-        table.concat(conditional_loads, '\n\t'),
-        'Condition for ' .. vim.inspect(names):gsub('"', "'")
-      )
-    )
   end
 
   local command_defs = {}

lua/packer/load.lua
@@ -1,85 +1,86 @@
 local packer_load = nil
-packer_load = function(names, cause, plugins)
-  local some_unloaded = false
-  local needs_bufread = false
-  local num_names = #names
-  local cmd = vim.api.nvim_command
-  local fmt = string.format
-  for i = 1, num_names do
-    local plugin = plugins[names[i]]
-    if not plugin then
-      local err_message = 'Error: attempted to load ' .. names[i] .. ' which is not present in plugins table!'
-      print(err_message)
-      error(err_message)
+local cmd = vim.api.nvim_command
+local fmt = string.format
+
+local function verify_conditions(conds, name)
+  if conds == nil then
+    return true
+  end
+  for _, cond in ipairs(conds) do
+    local success, result
+    if type(cond) == 'boolean' then
+      result = cond
+    elseif type(cond) == 'string' then
+      success, result = pcall(loadstring(cond))
+      if not success then
+        vim.schedule(function()
+          vim.api.nvim_notify(
+            'packer.nvim: Error running cond for ' .. name .. ': ' .. result,
+            vim.log.levels.ERROR,
+            {}
+          )
+        end)
+        return false
+      end
     end
+    if result == false then
+      return false
+    end
+  end
+  return true
+end
 
-    if not plugin.loaded then
-      -- Set the plugin as loaded before config is run in case something in the config tries to load
-      -- this same plugin again
-      plugin.loaded = true
-      some_unloaded = true
-      needs_bufread = needs_bufread or plugin.needs_bufread
-      if plugin.wants then
-        for _, wanted_name in ipairs(plugin.wants) do
-          packer_load({ wanted_name }, {}, plugins)
-        end
-      end
-
-      if plugin.commands then
-        for _, del_cmd in ipairs(plugin.commands) do
-          cmd('silent! delcommand ' .. del_cmd)
-        end
-      end
-
-      if plugin.keys then
-        for _, key in ipairs(plugin.keys) do
-          cmd(fmt('silent! %sunmap %s', key[1], key[2]))
-        end
-      end
-
-      cmd('packadd ' .. names[i])
-      if plugin.after_files then
-        for _, file in ipairs(plugin.after_files) do
-          cmd('silent source ' .. file)
-        end
-      end
-
-      if plugin.config then
-        for _, config_line in ipairs(plugin.config) do
-          local success, err = pcall(loadstring(config_line))
-          if not success then
-            vim.schedule(function()
-              vim.api.nvim_notify(
-                'packer.nvim: Error running config for ' .. names[i] .. ': ' .. err,
-                vim.log.levels.ERROR,
-                {}
-              )
-            end)
-          end
-        end
-      end
-
-      if plugin.after then
-        for _, after_name in ipairs(plugin.after) do
-          local after_plugin = plugins[after_name]
-          after_plugin.load_after[names[i]] = nil
-          if next(after_plugin.load_after) == nil then
-            packer_load({ after_name }, {}, plugins)
-          end
-        end
-      end
+local function loader_clear_loaders(plugin)
+  if plugin.commands then
+    for _, del_cmd in ipairs(plugin.commands) do
+      cmd('silent! delcommand ' .. del_cmd)
     end
   end
 
-  if not some_unloaded then
-    return
+  if plugin.keys then
+    for _, key in ipairs(plugin.keys) do
+      cmd(fmt('silent! %sunmap %s', key[1], key[2]))
+    end
   end
-  if needs_bufread then
-    cmd 'doautocmd BufRead'
+end
+
+local function loader_apply_config(plugin, name)
+  if plugin.config then
+    for _, config_line in ipairs(plugin.config) do
+      local success, err = pcall(loadstring(config_line))
+      if not success then
+        vim.schedule(function()
+          vim.api.nvim_notify('packer.nvim: Error running config for ' .. name .. ': ' .. err, vim.log.levels.ERROR, {})
+        end)
+      end
+    end
   end
+end
+
+local function loader_apply_wants(plugin, plugins)
+  if plugin.wants then
+    for _, wanted_name in ipairs(plugin.wants) do
+      packer_load({ wanted_name }, {}, plugins)
+    end
+  end
+end
+
+local function loader_apply_after(plugin, plugins, name)
+  if plugin.after then
+    for _, after_name in ipairs(plugin.after) do
+      local after_plugin = plugins[after_name]
+      after_plugin.load_after[name] = nil
+      if next(after_plugin.load_after) == nil then
+        packer_load({ after_name }, {}, plugins)
+      end
+    end
+  end
+end
+
+local function apply_cause_side_effcts(cause)
   if cause.cmd then
     local lines = cause.l1 == cause.l2 and '' or (cause.l1 .. ',' .. cause.l2)
-    vim.cmd(fmt('%s %s%s%s %s', cause.mods or '', lines, cause.cmd, cause.bang, cause.args))
+    cmd(fmt('%s %s%s%s %s', cause.mods or '', lines, cause.cmd, cause.bang, cause.args))
   elseif cause.keys then
     local extra = ''
     while true do
@@ -114,8 +115,51 @@ packer_load = function(names, cause, plugins)
   end
 end
 
-local function load_wrapper(names, cause, plugins)
-  local success, err_msg = pcall(packer_load, names, cause, plugins)
+packer_load = function(names, cause, plugins, force)
+  local some_unloaded = false
+  local needs_bufread = false
+  local num_names = #names
+  for i = 1, num_names do
+    local plugin = plugins[names[i]]
+    if not plugin then
+      local err_message = 'Error: attempted to load ' .. names[i] .. ' which is not present in plugins table!'
+      print(err_message)
+      error(err_message)
+    end
+
+    if not plugin.loaded then
+      loader_clear_loaders(plugin)
+      if force or verify_conditions(plugin.cond, names[i]) then
+        -- Set the plugin as loaded before config is run in case something in the config tries to load
+        -- this same plugin again
+        plugin.loaded = true
+        some_unloaded = true
+        needs_bufread = needs_bufread or plugin.needs_bufread
+        loader_apply_wants(plugin, plugins)
+        cmd('packadd ' .. names[i])
+        if plugin.after_files then
+          for _, file in ipairs(plugin.after_files) do
+            cmd('silent source ' .. file)
+          end
+        end
+        loader_apply_config(plugin, names[i])
+        loader_apply_after(plugin, plugins, names[i])
+      end
+    end
+  end
+
+  if not some_unloaded then
+    return
+  end
+  if needs_bufread then
+    cmd 'doautocmd BufRead'
+  end
+  -- Retrigger cmd/keymap...
+  apply_cause_side_effcts(cause)
+end
+
+local function load_wrapper(names, cause, plugins, force)
+  local success, err_msg = pcall(packer_load, names, cause, plugins, force)
   if not success then
     vim.cmd 'echohl ErrorMsg'
     vim.cmd('echomsg "Error in packer_compiled: ' .. vim.fn.escape(err_msg, '"') .. '"')

commit f7830f752089098bb361b359b3820807fa2da418

Fix #659: Allow space-separated strings of plugin names in packer.loader
This fixes a regression introduced in 755f7c4a1 by once again allowing packer.loader to accept lists of plugin names given as a single space-separated string/by including space-separated strings in the varargs given to packer.loader.

lua/packer.lua
@@ -745,12 +745,28 @@ end
 -- Load plugins
 -- @param plugins string String of space separated plugins names
 --                      intended for PackerLoad command
+--                or list of plugin names as independent strings
 packer.loader = function(...)
-  local plugin_list = { ... }
-  local force = plugin_list[#plugin_list] == true
-  if type(plugin_list[#plugin_list]) == 'boolean' then
-    plugin_list[#plugin_list] = nil
+  local plugin_names = { ... }
+  local force = plugin_names[#plugin_names] == true
+  if type(plugin_names[#plugin_names]) == 'boolean' then
+    plugin_names[#plugin_names] = nil
   end
+
+  -- We make a new table here because it's more convenient than expanding a space-separated string
+  -- into the existing plugin_names
+  local plugin_list = {}
+  for _, plugin_name in ipairs(plugin_names) do
+    vim.list_extend(
+      plugin_list,
+      vim.tbl_filter(function(name)
+        return #name > 0
+      end, vim.split(plugin_name, ' '))
+    )
+  end
+
+  print(vim.inspect(plugin_list))
+
   require 'packer.load'(plugin_list, {}, _G.packer_plugins, force)
 end
 