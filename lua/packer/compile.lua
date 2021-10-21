7b44861d00e0614326c38b1103f8c16a61abb0df
Omer Sarig <omer@sarig.co.il>
2021-10-21 21:40:55 +0300

    Pass name and plugin table to config function inside try_loadstring

--- lua/packer/compile.lua
+++ lua/packer/compile.lua
@@ -114,7 +114,7 @@ end
 
 local try_loadstring = [[
 local function try_loadstring(s, component, name)
-  local success, result = pcall(loadstring(s))
+  local success, result = pcall(loadstring(s), name, _G.packer_plugins[name])
   if not success then
     vim.schedule(function()
       vim.api.nvim_notify('packer.nvim: Error running ' .. component .. ' for ' .. name .. ': ' .. result, vim.log.levels.ERROR, {})
