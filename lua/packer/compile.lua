ec28f593b8e07d0d4e858abfe23a497c6f28eb35
doctoromer <omer@sarig.co.il>
2022-02-11 02:24:38 +0200

    Escape doublequotes to prevent cryptic errors while trying to report an error (#815)

--- lua/packer/compile.lua
+++ lua/packer/compile.lua
@@ -46,6 +46,7 @@ local catch_errors_lua = [[
 end)
 
 if not no_errors then
+  error_msg = error_msg:gsub('"', '\\"')
   vim.api.nvim_command('echohl ErrorMsg | echom "Error in packer_compiled: '..error_msg..'" | echom "Please check your config for correctness" | echohl None')
 end
 ]]
