ae95b6984d11dc2bb448527223be9a5298f04cc0
Wil Thomason <wil.thomason@gmail.com>
2022-04-16 01:30:04 -0500

    WIP everything-as-handlers-no-compile approach

--- lua/packer/handlers.lua
+++ lua/packer/handlers.lua
@@ -1,11 +1,222 @@
-local config = nil
+--- Handlers:
+--- A handler is a table minimally defining the following keys:
+---   name: a string uniquely naming the handler
+---   startup: a Boolean indicating if the handler needs to be applied at startup or only when plugins are being fully managed
+---   process(spec): a function which precomputes any information necessary to apply the handler for the given plugin spec
+---   apply(): a function which evaluates the handler's effect
+---   reset(): a function which clears all precomputed state from the handler
 
-local function cfg(_config)
-  config = _config
-end
+local M = {}
 
-local handlers = {
-  cfg = cfg,
+-- Default handlers are defined here rather than in their own modules to avoid needing to load a
+-- bunch of files on every start
+local handler_names = {
+  [module] = true,
+  cmd = true,
+  cond = true,
+  config = true,
+  disable = true,
+  event = true,
+  fn = true,
+  ft = true,
+  keys = true,
+  load_after = true,
+  requires = true,
+  rtp = true,
+  -- TODO: handle ftdetect stuff with caching?
+  setup = true,
 }
 
-return handlers
+-- Default handler implementations
+
+local profile = require 'packer.profile'
+local timed_run = profile.timed_run
+local timed_load = profile.timed_load
+
+-- Handler for the 'setup' key
+local setup_handler = { name = 'setup', startup = true, setups = {} }
+function setup_handler.reset()
+  setup_handler.setups = {}
+end
+
+function setup_handler.process(plugin)
+  if plugin.setup == nil then
+    plugin.only_setup = false
+    return
+  end
+
+  -- only_setup may already be false, but should never be true before now
+  plugin.only_setup = plugin.only_setup == nil
+  plugin.simple_load = false
+  setup_handler.setups[#setup_handler.setups + 1] = plugin
+end
+
+function setup_handler.apply()
+  local setups = setup_handler.setups
+  for i = 1, #setups do
+    local plugin = setups[i]
+    timed_run(plugin.setup, 'setup for ' .. plugin.short_name, plugin.short_name, plugin)
+    if plugin.only_setup then
+      timed_load(plugin.short_name)
+    end
+  end
+end
+
+-- Handler for the 'config' key
+local config_handler = { name = 'config', startup = true, configs = {} }
+function config_handler.reset()
+  config_handler.configs = {}
+end
+
+function config_handler.process(plugin)
+  if plugin.config then
+    plugin.simple_load = false
+    config_handler.configs[#config_handler.configs + 1] = plugin
+  end
+end
+
+function config_handler.apply()
+  local configs = config_handler.configs
+  for i = 1, #configs do
+    local plugin = configs[i]
+    timed_run(plugin.config, 'config for ' .. plugin.short_name, plugin.short_name, plugin)
+  end
+end
+
+-- Handler for the 'module' and 'module_pattern' keys
+local module_handler = { name = 'module', startup = true, modules = {}, lazy_load_called = { ['packer.load'] = true } }
+function module_handler.reset()
+  module_handler.modules = {}
+  module_handler.lazy_load_called = { ['packer.load'] = true }
+end
+
+local function lazy_load_module(module_name)
+  local to_load = {}
+  if module_handler.lazy_load_called[module_name] then
+    return nil
+  end
+
+  module_handler.lazy_load_called[module_name] = true
+  for module_pat, plugin in pairs(module_handler.modules) do
+    if not plugin.loaded and module_name:match(module_pat) then
+      to_load[#to_load + 1] = plugin
+    end
+  end
+
+  if #to_load > 0 then
+    -- TODO: Need to update packer.load to take plugin specs, not just names
+    require 'packer.load'(to_load, { module = module_name }, _G.packer_plugins)
+    local loaded_mod = package.loaded[module_name]
+    if loaded_mod then
+      return function(_)
+        return loaded_mod
+      end
+    end
+  end
+end
+
+function module_handler.process(plugin)
+  if plugin.module or plugin.module_pattern then
+    plugin.simple_load = false
+    plugin.only_sequence = false
+    plugin.only_setup = false
+    plugin.only_cond = false
+    if plugin.module then
+      if type(plugin.module) == 'string' then
+        plugin.module = { plugin.module }
+      end
+
+      for i = 1, #plugin.module do
+        module_handler.modules['^' .. vim.pesc(plugin.module[i])] = plugin
+      end
+    else
+      if type(plugin.module_pattern) == 'string' then
+        plugin.module_pattern = { plugin.module_pattern }
+      end
+
+      for i = 1, #plugin.module_pattern do
+        module_handler.modules[plugin.module_pattern[i]] = plugin
+      end
+    end
+  end
+end
+
+function module_handler.apply()
+  if #module_handler.modules > 0 then
+    if vim.g.packer_custom_loader_enabled then
+      package.loaders[1] = lazy_load_module
+    else
+      table.insert(package.loaders, 1, lazy_load_module)
+      vim.g.packer_custom_loader_enabled = true
+    end
+  end
+end
+
+local startup_handlers = {
+  module_handler,
+  cmd = true,
+  cond = true,
+  config_handler,
+  disable = true,
+  event = true,
+  fn = true,
+  ft = true,
+  keys = true,
+  load_after = true,
+  rtp = true,
+  setup_handler,
+}
+local num_startup = #startup_handlers
+local deferred_handlers = {
+  requires = true,
+}
+local num_deferred = #deferred_handlers
+
+function M.add(handler)
+  if handler_names[handler.name] ~= nil then
+    require('packer.log').warn('Duplicate handler "' .. handler.name .. '" added. Ignoring!')
+  elseif handler.startup then
+    num_startup = num_startup + 1
+    startup_handlers[num_startup] = handler
+    handler_names[handler.name] = true
+  else
+    num_deferred = num_deferred + 1
+    deferred_handlers[num_deferred] = handler
+    handler_names[handler.name] = true
+  end
+end
+
+function M.process_startup(plugin)
+  for i = 1, num_startup do
+    startup_handlers[i].process(plugin)
+  end
+end
+
+function M.apply_startup()
+  for i = 1, num_startup do
+    startup_handlers[i].apply()
+  end
+end
+
+function M.process_deferred(plugin)
+  for i = 1, num_deferred do
+    deferred_handlers[i].process(plugin)
+  end
+end
+
+function M.apply_deferred()
+  for i = 1, num_deferred do
+    deferred_handlers[i].apply()
+  end
+end
+
+-- TODO: Probably a function to get all handlers, to get a handler by its name, etc.
+function M.get_startup()
+  return startup_handlers
+end
+
+function M.get_deferred()
+  return deferred_handlers
+end
+
+return M
--- lua/packer/init.lua
+++ lua/packer/init.lua
@@ -3,52 +3,54 @@ local M = {}
 
 -- TODO: Investigate whether using FFI structs for the elements of these tables would be useful
 -- and/or faster for operations
-local plugins, plugin_specifications, rocks, profile_output, runtime_handlers
+local plugins, plugin_specifications, rocks, config
 
-local function is_runtime(handler)
-  return handler.runtime
-end
-
-local function register_default_runtime_handlers()
-  runtime_handlers = vim.tbl_filter(is_runtime, require('packer.handlers').default_handlers)
-end
+local handlers = require 'packer.handlers'
 
 local function ensure_dir(path)
   local path_info = vim.loop.fs_stat(path)
   if path_info == nil or path_info.type ~= 'directory' then
-    vim.fn.mkdir(path, 'p')
+    -- TODO: Investigate if rolling our own mkdir -p with vim.loop.fs_mkdir is faster
+    if vim.fn.mkdir(path, 'p') ~= 1 then
+      require('packer.log').warn("Couldn't create path: " .. path)
+    end
   end
 end
 
 --- Configure and reset packer
 function M.init(user_config)
-  local config = require('packer.config').configure(user_config)
+  if vim.fn.has 'nvim-0.7' ~= 1 then
+    require('packer.log').error 'Invalid Neovim version for packer.nvim!'
+    error 'Tried to use packer.nvim with Neovim <v0.7!'
+  end
+
+  config = require('packer.config').configure(user_config)
   M.reset()
   ensure_dir(config.opt_dir)
   ensure_dir(config.start_dir)
+  ensure_dir(config.snapshot_path)
+
   -- TODO: Maybe this should not go in init, but rather be separately called in startup?
   if not config.disable_commands then
     M.make_commands()
   end
-
-  register_default_runtime_handlers()
 end
 
 --- Reset packer's internal state
 function M.reset()
   plugin_specifications = {}
   plugins = {}
-  profile_output = {}
   rocks = {}
-  runtime_handlers = {}
 end
 
---- Utility function to ensure that an object is a table
 -- Redefined here from packer.util to avoid an unnecessary require
+--- Utility function to ensure that an object is a table
 local function ensure_table(obj)
   return (type(obj) == 'table' and obj) or { obj }
 end
 
+--- TODO: Should this be special-cased like this? It seems as though this might be better as a
+--- normal handler
 --- Add one or more Luarocks packages to the managed set
 --- Primarily designed for use with non-plugin-specific rocks
 ---@param rock_specifications string, full rock specification, or list of rock specifications
@@ -71,17 +73,40 @@ end
 --- Add a plugin specification key handler to the set which will be run on plugins
 ---@param handler table describing a handler object
 --- See packer.handlers for examples
-function M.add_handler(handler)
-  require('packer.handlers').add(handler)
-  -- TODO: What about checking for duplicate handlers?
-  if handler.runtime then
-    runtime_handlers[#runtime_handlers + 1] = handler
+M.add_handler = handlers.add
+
+local expand = vim.fn.expand
+local path_utils = require 'packer.path'
+local path_separator = path_utils.path_separator
+--- Get the canonical short name for a plugin. Defined here to avoid a require
+---@param plugin table plugin spec
+local function get_plugin_short_name(plugin)
+  local expanded_path = expand(plugin[1])
+  local name_segments = vim.split(expanded_path, path_separator)
+  local idx = #name_segments
+  -- NOTE: special-cased key here; should this be a handler?
+  local name = plugin.as or name_segments[idx]
+  while name == '' and idx > 0 do
+    name = name_segments[idx]
+    idx = idx - 1
   end
+
+  return name, expanded_path
+end
+
+--- Utility function to get the canonical names of top-level plugins from a table of specifications
+---@param specs table of plugin specs
+local function get_plugin_names(specs)
+  local names = {}
+  for i = 1, #specs do
+    names[#names + 1] = get_plugin_short_name(specs[i])
+  end
+
+  return names
 end
 
 --- Utility function to recursively flatten a potentially nested list of plugin specifications. Used by flatten_specification
 ---@param specs table of (potentially nested) plugin specifications
----@param from_requires boolean describing whether the current value of specs originated as a -requires key
 ---@param result table modified in place with the flattened list of specs
 local function flatten(specs, from_requires, result)
   local num_specs = #specs
@@ -92,6 +117,7 @@ local function flatten(specs, from_requires, result)
     if spec.requires then
       ensure_table(spec.requires)
       flatten(spec.requires, true)
+      spec.requires = get_plugin_names(spec.requires)
     end
   end
 end
@@ -99,25 +125,55 @@ end
 --- Recursively flatten a potentially nested list of plugin specifications
 ---@param plugin_specification string or full plugin specification or list of plugin specifications
 local function flatten_specification(plugin_specification)
-  if plugin_specification == nil then
-    return nil
-  end
-
   plugin_specification = ensure_table(plugin_specification)
   local result = {}
   flatten(plugin_specification, false, result)
   return result
 end
 
-local function process_runtime_handlers(plugin)
-  for i = 1, #runtime_handlers do
-    runtime_handlers[i].process(plugin)
-  end
-end
-
 --- Utility function responsible for consistently setting plugin metadata that may be used by
 --- handlers
-local function set_plugin_metadata(plugin) end
+local function set_plugin_metadata(plugin)
+  local name, path = get_plugin_short_name(plugin)
+  -- Check name/alias validity
+  if name == '' then
+    require('packer.log').warn([["]] .. plugin[1] .. [[" is an invalid plugin name!]])
+    return false
+  end
+
+  if plugins[name] and not plugins[name].from_requires then
+    require('packer.log').warn([[Plugin "]] .. name .. [[" is used twice! (line ]] .. plugin.line .. [[)]])
+    return false
+  end
+
+  if plugin.as and plugins[plugin.as] then
+    require('packer.log').error(
+      string.format(
+        [[The alias %s specified for %s at line %d is already used as a plugin name!]],
+        plugin.as,
+        path,
+        plugin.line
+      )
+    )
+    return false
+  end
+
+  -- Set plugin name and path
+  -- TODO: Eliminate use of plugin.name in the rest of the code and call plugin.short_name plugin.name
+  plugin.short_name = name
+  plugin.name = path
+  plugin.path = path
+
+  -- Set manual optness
+  if plugin.opt then
+    plugin.manual_opt = true
+  elseif plugin.opt == nil and config.opt_default then
+    plugin.manual_opt = true
+    plugin.opt = true
+  end
+
+  return true
+end
 
 local getinfo = debug.getinfo
 --- Add one or more plugin specifications to the managed set
@@ -129,14 +185,19 @@ function M.use(plugin_specification)
   local num_specs = #flattened_specification
   for i = 1, num_specs do
     local plugin = flattened_specification[i]
-    plugin_specifications[#plugin_specifications + 1] = {
-      spec = plugin,
-      line = current_line,
-      plugin_index = i,
-    }
+    if not plugin[1] then
+      require('packer.log').warn('No plugin name provided for spec at line ' .. current_line)
+    else
+      plugin_specifications[#plugin_specifications + 1] = {
+        spec = plugin,
+        line = current_line,
+        plugin_index = i,
+      }
 
-    set_plugin_metadata(plugin)
-    process_runtime_handlers(plugin)
+      set_plugin_metadata(plugin)
+      handlers.process_startup(plugin)
+      plugins[plugin.short_name] = plugin
+    end
   end
 end
 
@@ -231,8 +292,9 @@ end
 
 --- Show the output, if any exists, of packer's profiler
 function M.profile_output()
-  if profile_output then
-    require('packer.display').display_profile_output(profile_output)
+  local profiles = require('packer.profile').get_profile_data()
+  if profiles then
+    require('packer.display').display_profile_output(profiles)
   else
     local log = require 'packer.log'
     log.warn 'No profile output to display! Set config.profile.enable = true and restart'
@@ -243,7 +305,6 @@ end
 --- Takes varargs giving plugin names to load, as either a string of space separated names or a list
 --- of names as independent strings
 function M.activate_plugins(...)
-  ensure_all_plugins_managed()
   local plugin_names = { ... }
   local force = plugin_names[#plugin_names] == true
   if type(plugin_names[#plugin_names]) == 'boolean' then
@@ -268,7 +329,6 @@ end
 --- Completion for not-yet-loaded plugin names
 --- Used by PackerLoad command
 local function complete_loadable_plugin_names(lead, _, _)
-  ensure_all_plugins_managed()
   local completion_list = {}
   for name, plugin in pairs(plugins) do
     if vim.startswith(name, lead) and not plugin.loaded then
@@ -282,7 +342,6 @@ end
 --- Completion for managed plugin names
 --- Used by PackerInstall/Update/Sync commands
 local function complete_plugin_names(lead, _, _)
-  ensure_all_plugins_managed()
   local completion_list = vim.tbl_filter(function(name)
     return vim.startswith(name, lead)
   end, vim.tbl_keys(plugins))
