f6031326267f7728b6058458cec69d4de98b4844
Wil Thomason <wil.thomason@gmail.com>
2022-04-16 01:28:07 -0500

    Make profile lib from compile functions

---
+++ lua/packer/profile.lua
@@ -0,0 +1,68 @@
+--- Support for fine-grained profiling of startup steps
+local M = { results = {} }
+
+local function time(name, is_start) end
+
+local profile_data = {}
+local hrtime = vim.loop.hrtime
+local threshold = nil
+require('packer.config').register_hook(function(config)
+  threshold = config.profile.threshold
+  if config.profile.enable then
+    time = function(name, is_start)
+      if is_start then
+        profile_data[name] = hrtime()
+      else
+        profile_data[name] = (hrtime() - profile_data[name]) / 1e6
+      end
+    end
+  end
+end)
+
+function M.save_profiles()
+  M.results = {}
+  local sorted_times = {}
+  for chunk_name, time_taken in pairs(profile_data) do
+    sorted_times[#sorted_times + 1] = { chunk_name, time_taken }
+  end
+
+  table.sort(sorted_times, function(a, b)
+    return a[2] > b[2]
+  end)
+
+  for i, elem in ipairs(sorted_times) do
+    if not threshold or threshold and elem[2] > threshold then
+      M.results[i] = elem[1] .. ' took ' .. elem[2] .. 'ms'
+    end
+  end
+end
+
+function M.get_profile_data()
+  if #M.results == 0 then
+    M.save_profiles()
+  end
+
+  return M.results
+end
+
+function M.timed_run(fn, name, ...)
+  time(name, true)
+  local success, result = pcall(fn, ...)
+  time(name, false)
+  if not success then
+    vim.schedule(function()
+      require('packer.log').error('Failed running ' .. name .. ': ' .. result)
+    end)
+  end
+
+  return result
+end
+
+function M.timed_load(name)
+  local packadd_cmd = 'packadd ' .. name
+  time(packadd_cmd, true)
+  vim.cmd(packadd_cmd)
+  time(packadd_cmd, false)
+end
+
+return M
