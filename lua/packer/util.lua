0b3cd21246f97edff13266d01012340c49d6b412
Lewis Russell <lewis6991@gmail.com>
2022-11-27 12:22:03 +0000

    fixup! remove util.float

--- lua/packer/util.lua
+++ lua/packer/util.lua
@@ -47,37 +47,4 @@ function util.join_paths(...)
    return table.concat({ ... }, util.get_separator())
 end
 
-
-function util.float(opts)
-   local last_win = vim.api.nvim_get_current_win()
-   local last_pos = vim.api.nvim_win_get_cursor(last_win)
-   local columns = vim.o.columns
-   local lines = vim.o.lines
-   local width = math.ceil(columns * 0.8)
-   local height = math.ceil(lines * 0.8 - 4)
-   local left = math.ceil((columns - width) * 0.5)
-   local top = math.ceil((lines - height) * 0.5 - 1)
-
-   local buf = vim.api.nvim_create_buf(false, true)
-   local win = vim.api.nvim_open_win(buf, true, vim.tbl_deep_extend('force', {
-      relative = 'editor',
-      style = 'minimal',
-      border = 'double',
-      width = width,
-      height = height,
-      col = left,
-      row = top,
-   }, opts))
-
-   vim.api.nvim_create_autocmd('BufWipeout', {
-      buffer = buf,
-      callback = function()
-         vim.api.nvim_set_current_win(last_win)
-         vim.api.nvim_win_set_cursor(last_win, last_pos)
-      end,
-   })
-
-   return true, win, buf
-end
-
 return util
