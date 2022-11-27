236dafb307c1c32432d20b048d4ae56489cd786e
Lewis Russell <lewis6991@gmail.com>
2022-11-27 10:48:56 +0000

    fix: add missing lua file

---
+++ lua/packer/handlers/enable.lua
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+return function(plugins, loader)
+   for _, plugin in pairs(plugins) do
+      local enable = plugin.enable
+      if type(enable) == "function" then
+         if enable() then
+            loader({ plugin })
+         end
+      elseif enable then
+         loader({ plugin })
+      end
+   end
+end
