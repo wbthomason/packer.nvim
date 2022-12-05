1d8babfcce82197b6388d6c0c45937dcef2a072e
Wil Thomason <wil.thomason@gmail.com>
2022-12-04 22:24:18 -0600

    Revert "feat(compile): use nvim_create_user_command to make lazy-load commands (#1149)"
    This (temporarily) reverts commit 07c79941e129f5d5e200e1da12ff1f082b812e92 until we can fix the bug noted by @kevinhwang91.

--- lua/packer/compile.lua
+++ lua/packer/compile.lua
@@ -570,17 +570,8 @@ local function make_loaders(_, plugins, output_lua, should_profile)
   for command, names in pairs(commands) do
     local command_line
     if string.match(command, '^%w+$') then
-      -- Better command completions here are due to @folke
       command_line = fmt(
-        [[pcall(vim.api.nvim_create_user_command, '%s', function(cmdargs)
-          require('packer.load')({%s}, { cmd = '%s', l1 = cmdargs.line1, l2 = cmdargs.line2, bang = cmdargs.bang, args = cmdargs.args, mods = cmdargs.mods }, _G.packer_plugins)
-        end,
-        {nargs = '*', range = true, bang = true, complete = function()
-          require('packer.load')({%s}, { cmd = '%s' }, _G.packer_plugins)
-          vim.api.nvim_input('<space><bs><tab>')
-      end})]],
-        command,
-        table.concat(names, ', '),
+        'pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file %s lua require("packer.load")({%s}, { cmd = "%s", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])',
         command,
         table.concat(names, ', '),
         command
